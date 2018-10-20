225b06badf8ac2cfca22ae18ff8c310380967cc6
Quetzy Garcia <quetzyg@altek.org>
2018-10-21 00:26:09 +0100

    feat(Accountant): added default configuration

---
+++ config/accountant.php
@@ -0,0 +1,101 @@
+<?php
+
+return [
+
+    'ledger' => [
+
+        /*
+        |--------------------------------------------------------------------------
+        | Ledger Implementation
+        |--------------------------------------------------------------------------
+        |
+        | Define the Ledger implementation.
+        |
+        */
+
+        'implementation' => Altek\Accountant\Models\Ledger::class,
+
+        /*
+        |--------------------------------------------------------------------------
+        | Ledger Resolver Implementations
+        |--------------------------------------------------------------------------
+        |
+        | Define the User, IP Address, User Agent and URL resolver implementations.
+        |
+        */
+        'resolvers' => [
+            'user'       => Altek\Accountant\Resolvers\UserResolver::class,
+            'ip_address' => Altek\Accountant\Resolvers\IpAddressResolver::class,
+            'user_agent' => Altek\Accountant\Resolvers\UserAgentResolver::class,
+            'url'        => Altek\Accountant\Resolvers\UrlResolver::class,
+        ],
+
+        /*
+        |--------------------------------------------------------------------------
+        | Ledger Events
+        |--------------------------------------------------------------------------
+        |
+        | The events that should give origin to a new Ledger record.
+        |
+        */
+
+        'events' => [
+            'created',
+            'updated',
+            'deleted',
+            'restored',
+        ],
+
+        /*
+        |--------------------------------------------------------------------------
+        | CLI Recording
+        |--------------------------------------------------------------------------
+        |
+        | Whether a Ledger should be created from Command Line Interface events.
+        |
+        */
+
+        'cli' => false,
+
+        /*
+        |--------------------------------------------------------------------------
+        | Ledger Threshold
+        |--------------------------------------------------------------------------
+        |
+        | Specify a cutoff for the number of Ledger records a model can have.
+        | Zero means unlimited.
+        |
+        */
+
+        'threshold' => 0,
+
+        /*
+        |--------------------------------------------------------------------------
+        | Ledger Driver
+        |--------------------------------------------------------------------------
+        |
+        | The default driver used to keep track of changes.
+        |
+        */
+
+        'driver' => 'database',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | User MorphTo relation prefix & default Guards
+    |--------------------------------------------------------------------------
+    |
+    | Define the morph prefix and authentication guards for the User resolver.
+    |
+    */
+
+    'user' => [
+        'prefix' => 'user',
+        'guards' => [
+            'web',
+            'api',
+        ],
+    ],
+
+];
