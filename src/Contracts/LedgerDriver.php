8a1dd2e153332d679c7824b907e09732bde1cb23
Quetzy Garcia <quetzyg@altek.org>
2018-10-21 10:35:49 +0100

    feat(Ledger): added LedgerDriver interface + initial database driver implementation

---
+++ src/Contracts/LedgerDriver.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Altek\Accountant\Contracts;
+
+interface LedgerDriver
+{
+    /**
+     * Create a Ledger from a Recordable model.
+     *
+     * @param \Altek\Accountant\Contracts\Recordable $model
+     * @param string                                 $event
+     *
+     * @throws \Altek\Accountant\Exceptions\AccountantException
+     *
+     * @return \Altek\Accountant\Contracts\Ledger
+     */
+    public function record(Recordable $model, string $event): Ledger;
+
+    /**
+     * Remove older ledgers that go over the threshold.
+     *
+     * @param \Altek\Accountant\Contracts\Recordable $model
+     *
+     * @return bool
+     */
+    public function prune(Recordable $model): bool;
+}
---
+++ src/Drivers/Database.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Altek\Accountant\Drivers;
+
+use Altek\Accountant\Contracts\Ledger;
+use Altek\Accountant\Contracts\LedgerDriver;
+use Altek\Accountant\Contracts\Recordable;
+use Illuminate\Support\Facades\Config;
+
+class Database implements LedgerDriver
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function record(Recordable $model, string $event): Ledger
+    {
+        $implementation = Config::get('accountant.ledger.implementation', \Altek\Accountant\Models\Ledger::class);
+
+        return $implementation->create($model->toLedger($event));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function prune(Recordable $model): bool
+    {
+        if (($threshold = $model->getLedgerThreshold()) > 0) {
+            $forRemoval = $model->ledgers()
+                ->latest()
+                ->get()
+                ->slice($threshold)
+                ->pluck('id');
+
+            if (!$forRemoval->isEmpty()) {
+                return $model->ledgers()
+                    ->whereIn('id', $forRemoval)
+                    ->delete() > 0;
+            }
+        }
+
+        return false;
+    }
+}
