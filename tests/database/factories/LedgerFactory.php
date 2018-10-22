6d620c4a5cd80863f9ceb5c9a654c2e7c134a055
Quetzy Garcia <quetzyg@altek.org>
2018-10-22 09:49:00 +0100

    feat(Tests): added model factories

---
+++ tests/database/factories/LedgerFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+use Faker\Generator as Faker;
+use Altek\Accountant\Models\Ledger;
+use Altek\Accountant\Tests\Models\Article;
+use Altek\Accountant\Tests\Models\User;
+
+/*
+|--------------------------------------------------------------------------
+| Ledger Factories
+|--------------------------------------------------------------------------
+|
+*/
+
+$factory->define(Ledger::class, function (Faker $faker) {
+    return [
+        'user_id' => function () {
+            return factory(User::class)->create()->id;
+        },
+        'user_type'     => User::class,
+        'event'         => 'updated',
+        'recordable_id' => function () {
+            return factory(Article::class)->create()->id;
+        },
+        'recordable_type' => Article::class,
+        'properties'      => [],
+        'modified'        => [],
+        'url'             => $faker->url,
+        'ip_address'      => $faker->ipv4,
+        'user_agent'      => $faker->userAgent,
+    ];
+});
