04d944d5bec7386c2518b7af00b24b77755955c4
Quetzy Garcia <quetzyg@altek.org>
2018-11-03 13:04:17 +0000

    feat(Ciphers): added interface

---
+++ src/Contracts/Cipher.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Altek\Accountant\Contracts;
+
+interface Cipher
+{
+    /**
+     * Is this a one way cipher implementation?
+     *
+     * @return bool
+     */
+    public static function isOneWay(): bool;
+
+    /**
+     * Cipher a property value.
+     *
+     * @param mixed $value
+     *
+     * @return mixed
+     */
+    public static function cipher($value);
+
+    /**
+     * Decipher a property value.
+     *
+     * @param mixed $value
+     *
+     * @throws \Altek\Accountant\Exceptions\AccountantException
+     *
+     * @return mixed
+     */
+    public static function decipher($value);
+}
