18e41dc2ffa4d92e34e5d6518804157371099839
Quetzy Garcia <quetzyg@altek.org>
2018-11-21 23:11:25 +0000

    feat(CommandTest): add configuration/migration publishing tests

--- tests/Integration/CommandTest.php
+++ tests/Integration/CommandTest.php
@@ -9,9 +9,11 @@ class CommandTest extends AccountantTestCase
     /**
      * @test
      */
-    public function itWillCreateALedgerDriver(): void
+    public function itCreatesLedgerDriverSkeleton(): void
     {
-        $driverFilePath = sprintf('%s/LedgerDrivers/TestDriver.php', $this->app->path());
+        $driverFilePath = $this->app->path('LedgerDrivers/TestDriver.php');
+
+        $this->assertFileNotExists($driverFilePath);
 
         $this->artisan('make:ledger-driver', [
             'name' => 'TestDriver',
@@ -20,5 +22,41 @@ class CommandTest extends AccountantTestCase
         $this->assertFileExists($driverFilePath);
 
         $this->assertTrue(unlink($driverFilePath));
+    }
+
+    /**
+     * @test
+     */
+    public function itPublishesConfigurationFile(): void
+    {
+        $configurationFilePath = $this->app->configPath('accountant.php');
+
+        $this->assertFileNotExists($configurationFilePath);
+
+        $this->artisan('vendor:publish', [
+            '--tag' => 'accountant-configuration',
+        ]);
+
+        $this->assertFileExists($configurationFilePath);
+
+        $this->assertTrue(unlink($configurationFilePath));
+    }
+
+    /**
+     * @test
+     */
+    public function itPublishesMigrationFile(): void
+    {
+        $migrationFilePath = $this->app->databasePath('migrations/2018_11_21_000001_create_ledgers_table.php');
+
+        $this->assertFileNotExists($migrationFilePath);
+
+        $this->artisan('vendor:publish', [
+            '--tag' => 'accountant-migration',
+        ]);
+
+        $this->assertFileExists($migrationFilePath);
+
+        $this->assertTrue(unlink($migrationFilePath));
     }
 }
