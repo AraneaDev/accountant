92275d5f2a1eacba08170621eb9d37a43dca352f
Quetzy Garcia <quetzyg@altek.org>
2018-11-30 19:21:56 +0000

    feat(LedgerDriver): refactored Database driver record() method

--- src/Drivers/Database.php
+++ src/Drivers/Database.php
@@ -4,7 +4,9 @@ namespace Altek\Accountant\Drivers;
 
 use Altek\Accountant\Contracts\Ledger;
 use Altek\Accountant\Contracts\LedgerDriver;
+use Altek\Accountant\Contracts\LedgerSigner;
 use Altek\Accountant\Contracts\Recordable;
+use Altek\Accountant\Exceptions\AccountantException;
 use Illuminate\Support\Facades\Config;
 
 class Database implements LedgerDriver
@@ -16,7 +18,26 @@ class Database implements LedgerDriver
     {
         $implementation = Config::get('accountant.ledger.implementation', \Altek\Accountant\Models\Ledger::class);
 
-        return call_user_func([$implementation, 'create'], $model->process($event));
+        if (!is_subclass_of($implementation, Ledger::class)) {
+            throw new AccountantException(sprintf('Invalid Ledger implementation: "%s"', $implementation));
+        }
+
+        $ledger = new $implementation();
+
+        foreach ($data = $model->process($event) as $key => $value) {
+            $ledger->setAttribute($key, $value);
+        }
+
+        $signer = Config::get('accountant.ledger.signer', \Altek\Accountant\Signers\LedgerSigner::class);
+
+        if (!is_subclass_of($signer, LedgerSigner::class)) {
+            throw new AccountantException(sprintf('Invalid LedgerSigner implementation: "%s"', $signer));
+        }
+
+        $ledger->setAttribute('signature', call_user_func([$signer, 'sign'], $data))
+            ->save();
+
+        return $ledger;
     }
 
     /**
