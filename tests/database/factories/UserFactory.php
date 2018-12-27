260e30959f4b51c875aab4e7153ec5d01a6e1359
Quetzy Garcia <quetzyg@altek.org>
2018-12-27 03:30:49 +0000

    feat(UserFactory): use boolean() instead of randomElement()

--- tests/database/factories/UserFactory.php
+++ tests/database/factories/UserFactory.php
@@ -14,7 +14,7 @@ use Faker\Generator as Faker;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'is_admin'   => $faker->randomElement([0, 1]),
+        'is_admin'   => $faker->boolean(),
         'first_name' => $faker->firstName,
         'last_name'  => $faker->lastName,
         'email'      => $faker->unique()->safeEmail,
