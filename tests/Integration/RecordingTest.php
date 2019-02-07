2bc915f0628d02ab6ad17152b55aef93b9cb90c9
Quetzy Garcia <quetzyg@altek.org>
2019-02-07 18:16:16 +0000

    fix(RecordingTest): minor update due to a change in the altek/eventually package

--- tests/Integration/RecordingTest.php
+++ tests/Integration/RecordingTest.php
@@ -630,11 +630,11 @@ class RecordingTest extends AccountantTestCase
             'properties' => [
                 [
                     'user_id'    => 1,
-                    'article_id' => 1,
+                    'article_id' => '1',
                 ],
                 [
                     'user_id'    => 1,
-                    'article_id' => 2,
+                    'article_id' => '2',
                 ],
             ],
         ], $ledger->getPivotData(), true);
